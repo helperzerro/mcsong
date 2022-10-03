5a4278f381cd386a819d7be54673d6285d0d56ea
Helperzerro <wilmars2710@gmail.com>
2022-10-03 23:37:12 +0700

    Proses Pertama

---
+++ pages/index.js
@@ -0,0 +1,138 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center border border-red-400">
+      <iframe width="420" height="315" className="rounded-md my-4"
+      src="https://www.youtube.com/embed/xj_C5Iscqig">
+      </iframe>
+
+      <p className="bg-gradient-to-r from-red-500 to-rose-400 p-12 rounded-xl text-xl">
+        xiá mǎn tiān <br /> 
+	      霞  满  天 <br />  
+        <br /> 
+	      yì qǐ kàn wǎn xiá mǎn tiān <br /> 
+        一 起 看  晚  霞  满  天  <br />
+        <br /> 
+        dāng bì shàng shuāng yǎn <br /> 
+        当   闭 上    双     眼  <br />
+        <br /> 
+        hái wàng fǎn liú lián <br /> 
+        还  忘   返  流  连   <br /> 
+        <br /> 
+        wèi wán de xīn yuàn <br /> 
+        未  完  的 心  愿   <br /> 
+        <br /> 
+        yī wēi zài nǐ shēn biān <br /> 
+        依 偎  在  你 身   边   <br /> 
+        <br /> 
+        céng shì wǒ chōng jǐng de míng tiān <br /> 
+        曾   是  我 憧    憬   的 明   天   <br /> 
+        <br /> 
+        jiù huà zuò yì lǚ zǐ yān <br /> 
+        就  化  作  一 缕 紫 烟  <br /> 
+        <br /> 
+        huán rào nǐ xīn jiān <br /> 
+        环   绕  你 心  间   <br /> 
+        <br /> 
+        ruò néng zài duō kàn yì yǎn <br /> 
+        若  能   再  多  看  一 眼  <br /> 
+        <br /> 
+        gǔn tàng de xīn <br /> 
+        滚  烫   的 心  <br /> 
+        <br /> 
+        zhì rè de yǎn <br /> 
+        炙  热 的 眼  <br /> 
+        <br /> 
+        nǐ de lèi dī luò de shùn jiān <br /> 
+        你 的 泪  滴 落  的 瞬   间<br /> 
+        <br /> 
+        huá guò wǒ de liǎn <br /> 
+        滑  过  我 的 脸   <br /> 
+        <br /> 
+        hái méi kāi shǐ què dào zài jiàn<br />  
+        还  没  开  始  却  道  再  见   <br /> 
+        <br /> 
+        jiù suàn lái bù jí xiāng liàn <br /> 
+        就  算   来  不 及 相    恋   <br /> 
+        <br /> 
+        chà nà zhī jiān <br /> 
+        刹  那 之  间   <br /> 
+        <br /> 
+        juān kè yóng yuǎn <br /> 
+        镌   刻 永   远   <br /> 
+        <br /> 
+        zuó rì de qíng jǐng zài fú xiàn <br /> 
+        昨  日 的 情   景   再  浮 现   <br /> 
+        <br /> 
+        liú dàng zài xīn dǐ <br /> 
+        流  荡   在  心  底 <br /> 
+        <br /> 
+        huà yì lǚ yān piāo sàn <br /> 
+        化  一 缕 烟  飘   散  <br /> 
+        <br /> 
+        xiá mǎn tiān <br /> 
+        霞  满  天   <br /> 
+        <br /> 
+        yì qǐ kàn wǎn xiá mǎn tiān <br /> 
+        一 起 看  晚  霞  满  天  <br /> 
+        <br /> 
+        dāng bì shàng shuāng yǎn <br /> 
+        当   闭 上    双     眼  <br /> 
+        <br /> 
+        hái wàng fǎn liú lián <br /> 
+        还  忘   返  流  连   <br /> 
+        <br /> 
+        wèi wán de xīn yuàn <br /> 
+        未  完  的 心  愿   <br /> 
+        <br /> 
+        yī wēi zài nǐ shēn biān <br /> 
+        依 偎  在  你 身   边   <br /> 
+        <br /> 
+        céng shì wǒ chōng jǐng de míng tiān <br /> 
+        曾   是  我 憧    憬   的 明   天   <br /> 
+        <br /> 
+        jiù huà zuò yì lǚ zǐ yān <br /> 
+        就  化  作  一 缕 紫 烟  <br /> 
+        <br /> 
+        huán rào nǐ xīn jiān <br /> 
+        环   绕  你 心  间   <br /> 
+        <br /> 
+        lái bù jí hǎo hǎo gào bié <br /> 
+        来  不 及 好  好  告  别  <br /> 
+        <br /> 
+        kōng liú yí duàn <br /> 
+        空   留  一 段   <br /> 
+        <br /> 
+        jì yì de xiàn <br /> 
+        记 忆 的 线   <br /> 
+        <br /> 
+        xì bú xià cháng cháng de āi liàn <br /> 
+        系 不 下  长    长    的 哀 恋   <br /> 
+        <br /> 
+        què hún rào mèng qiān <br /> 
+        却  魂  绕  梦   牵   <br /> 
+        <br /> 
+        huǎng hū zhōng yòu hé nǐ xiāng jiàn <br /> 
+        恍    惚 中    又  和 你 相    见   <br /> 
+        <br /> 
+        yóng yuǎn dào dǐ yǒu duō yuǎn <br /> 
+        永   远   到  底 有  多  远   <br /> 
+        <br /> 
+        xīn xīn zhī jiān <br /> 
+        心  心  之  间   <br /> 
+        <br /> 
+        niàn niàn zhī yuǎn <br /> 
+        念   念   之  远   <br /> 
+        <br /> 
+        cǎi yí piàn wǎn xiá fàng xīn jiān <br /> 
+        采  一 片   晚  霞  放   心  间   <br /> 
+        <br /> 
+        nà shì wǒ duì nǐ <br /> 
+        那 是  我 对  你 <br /> 
+        <br /> 
+        zuì hòu de juàn liàn <br /> 
+        最  后  的 眷   恋   <br /> 
+      </p>
+      
+    </div>
+  )
+}
