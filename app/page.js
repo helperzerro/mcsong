fd2d188b880b8e633132afa3ae7a7ba94b0cda79
Helperzerro <wilmars2710@gmail.com>
2023-07-14 10:38:27 +0700

    bug, memperbaiki link hover error

--- app/page.js
+++ app/page.js
@@ -11,23 +11,23 @@ export default function Home() {
 
   return (
     <div className="items-center flex justify-center py-4">
-      <table className="w-[300px] border-separate border border-slate-500  bg-gray-700 text-white">
-        <thead className="bg-gray-800">
+      <table className="w-[450px] border-separate border border-slate-500  bg-gray-700 text-white">
+        <thead className="bg-gray-800 ">
           <tr className="border border-white ">
             <th className="border border-white">No</th>
-            <th className="border border-white">Singer</th>
-            <th className="border border-white">Title</th>
+            <th className="w-[150px] border border-white">Singer</th>
+            <th className="w-[250px] border border-white">Title</th>
           </tr>
         </thead>
 
         <tbody>
           {Object.values(lirikData).map((item, index) => (
-            <tr key={index} className="text-center">
-              <td className="border border-white">{index + 1}</td>
+            <tr key={index} className="text-center ">
+              <td className="border border-white ">{index + 1}</td>
               <td className="border border-white">{item.penyanyi}</td>
               <td className="border border-white">
                 <Link
-                  className="hover:scale-100 hover:font-bold transition"
+                  className="transition hover:scale-50 hover:font-bold hover:text-shadow border"
                   href={`/judul/${item.pinyinJudul}`}
                 >
                   {item.judul}
