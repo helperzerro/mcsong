c13b20580d4148ba647db884b34366cea8caae63
Helperzerro <wilmars2710@gmail.com>
2024-02-12 18:46:22 +0700

    tambah lagu

--- app/Footer.js
+++ app/Footer.js
@@ -3,32 +3,151 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="text-center text-white bg-gray-900 pb-5 pt-1">
-      <div className="flex flex-wrap justify-center items-center gap-x-4 gap-y-1 mb-3 mt-1">
-        <Link href="" className="transition hover:scale-125 ">
-          Advertise
-        </Link>
-        <Link href="" className="transition  hover:scale-125 ">
-          Disclaimer
-        </Link>
-        <Link href="" className="transition hover:scale-125 ">
-          Privacy
-        </Link>
-        <Link href="" className="transition hover:scale-125 ">
-          Policy
-        </Link>
-        <Link href="" className="transition hover:scale-125 ">
-          About Us
-        </Link>
-        <Link href="" className="transition hover:scale-125 ">
-          Youtube
-        </Link>
-        <Link href="" className="transition hover:scale-125 ">
-          DCSLyrics
-        </Link>
+    <div className="p-4 md:px-10 bg-black">
+      <div className="flex flex-col lg:flex-row lg:pt-8 container">
+        {/* Content 1 */}
+        <div className="text-white my-14 lg:my-0 lg:w-[650px]  xl:w-8/12">
+          <div>
+            <h3 className="font-bold text-3xl mb-6">GitHub</h3>
+          </div>
+          <div className="flex flex-col mb-4">
+            <span>Subscribe to our newsletter</span>
+            <span className="text-gray-400 opacity-85">
+              Get product updates, company news, and more.
+            </span>
+          </div>
+          <div>
+            <button className="border px-6 py-2.5 font-bold rounded-md border-gray-500">
+              Subscribe
+            </button>
+          </div>
+        </div>
+        {/* Close Content 1 */}
+
+        {/* Content 2 */}
+        <div className="grid grid-cols-2 gap-10 mb-8 text-gray-400 sm:grid-cols-4 w-full">
+          <div className="flex flex-col">
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              The ReadME Project
+            </Link>
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+          </div>
+          <div className="flex flex-col">
+            <Link href="" className="mb-2">
+              Advertise
+            </Link>
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>
+            <Link href="" className="mb-2">
+              Product
+            </Link>
+          </div>
+          <div className="flex flex-col">
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>
+          </div>
+          <div className="flex flex-col">
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>{" "}
+            <Link href="" className="mb-2">
+              Features
+            </Link>
+            <Link href="" className="mb-2">
+              Security
+            </Link>
+          </div>
+        </div>
+        {/* Close Content 2 */}
       </div>
 
-      <span className="">Copyright &#169; 2023 mcsong.vercel.app</span>
+      {/* Content 3 */}
+      <div className="text-gray-400">
+        <div>
+          <Link href="" className="">
+            Advertise
+          </Link>
+        </div>
+        <div>
+          <Link href="" className="">
+            Advertise
+          </Link>
+        </div>
+      </div>
+      {/* Close Content3 */}
     </div>
   );
 }
--- app/Navbar.js
+++ app/Navbar.js
@@ -5,10 +5,16 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FaHamburger, FaSearch } from "react-icons/fa";
 import { GiCrossedBones } from "react-icons/gi";
+{
+  /* Contoh Penulisan : <button className={`hamburger ${isOpen ? 'is-active' : ''}`} onClick={handleClick}> */
+}
 
 export default function Navbar() {
   const router = useRouter();
   const [showMenuItem, setMenuItem] = useState(false);
+  const [isOpenDroplink1, setIsOpenDroplink1] = useState(false);
+  const [isOpenDroplink2, setIsOpenDroplink2] = useState(false);
+  const [isOpenDroplink3, setIsOpenDroplink3] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (event) => {
@@ -23,9 +29,18 @@ export default function Navbar() {
 
   return (
     <div className="w-full bg-gray-900 ">
-      <nav class="mx-auto container sm:flex-row sm:flex sm:items-center sm:justify-between ">
-        <div class="flex justify-between items-center max-w-screen-xl px-4 md:px-6 py-2.5 sm:w-3/4">
-          {/* Icon */}
+      {/* Navbar Mobile Site*/}
+      <div class="mx-auto flex justify-between items-center sm:px-10 px-4 py-2.5 lg:hidden ">
+        {/* Sign Up Button */}
+        <div>
+          <button className="text-sm border px-2 py-1 rounded-md text-white">
+            Sign up
+          </button>
+        </div>
+        {/* Close Sign Up Button */}
+
+        {/* Icon App */}
+        <div>
           <Link href="http://mcsong.vercel.app" class="flex items-center">
             <Image
               src={"https://flowbite.com/docs/images/logo.svg"}
@@ -34,78 +49,216 @@ export default function Navbar() {
               height={20}
               className="mr-2"
             />
-            <span class="self-center text-xl font-semibold whitespace-nowrap text-white">
-              MCSONG
-            </span>
           </Link>
+        </div>
+        {/* Close Icon App */}
 
-          {/* Contoh Penulisan : <button className={`hamburger ${isOpen ? 'is-active' : ''}`} onClick={handleClick}> */}
-          <div
-            class={`${
-              showMenuItem ? "items-center " : "items-center hidden sm:flex"
-            }`}
-          >
-            <Link
-              href="/judul/人是"
-              class="mr-6 text-lg font-medium text-gray-500 dark:text-white hover:scale-125 transition"
-            >
-              人是
-            </Link>
-            <Link
-              href="/judul/紫"
-              class="text-lg font-medium text-blue-600 dark:text-blue-500 hover:scale-125 transition"
-            >
-              紫
+        {/* Humbeger Menu */}
+        {showMenuItem ? (
+          <>
+            <div className="flex flex-row items-center lg:hidden">
+              <button
+                className="lg:hidden"
+                onClick={() => {
+                  setMenuItem(!showMenuItem);
+                }}
+              >
+                <GiCrossedBones size={25} color="white" className="ml-3" />
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-row items-center lg:hidden">
+              <button
+                className="lg:hidden"
+                onClick={() => {
+                  setMenuItem(!showMenuItem);
+                }}
+              >
+                <FaHamburger size={25} color="white" className="ml-3" />
+              </button>
+            </div>
+          </>
+        )}
+        {/* Close Humbeger Menu  */}
+      </div>
+      {/* Close Navbar Mobile Site */}
+
+      {/* Navbar Website Site */}
+      <div class="mx-auto justify-between items-center lg:px-4 py-2.5 hidden lg:flex bg-gray-800">
+        <div className="flex">
+          {/* Icon App */}
+          <div>
+            <Link href="http://mcsong.vercel.app" class="flex items-center">
+              <Image
+                src={"https://flowbite.com/docs/images/logo.svg"}
+                alt="Logo"
+                width={40}
+                height={20}
+                className="mr-2"
+              />
             </Link>
           </div>
+          {/* Close Icon App */}
 
-          {showMenuItem ? (
-            <>
-              <div className="flex flex-row items-center sm:hidden">
-                <button
-                  className="sm:hidden"
-                  onClick={() => {
-                    setMenuItem(!showMenuItem);
-                  }}
-                >
-                  <GiCrossedBones size={25} color="white" className="ml-3" />
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="flex flex-row items-center sm:hidden">
-                <button
-                  className="sm:hidden"
-                  onClick={() => {
-                    setMenuItem(!showMenuItem);
-                  }}
-                >
-                  <FaHamburger size={25} color="white" className="ml-3" />
-                </button>
-              </div>
-            </>
-          )}
-        </div>
-
-        <div className="sm:w-64 lg:w-80 py-3 px-4 ">
-          <form
-            className="flex flex-row items-center relative"
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="h-9 pl-10 pr-2 text-sm text-white border-2 border-gray-300 rounded-lg bg-transparent absolute right-0 left-0"
-              placeholder="Judul lagu..."
-              onChange={handleInputChange}
-              value={inputValue}
-              type="text"
-            />
-            <button className="z-10" type="submit">
-              <FaSearch size={25} color="white" className="ml-2" />
+          {/* Dropdown Link */}
+          <div className="relative">
+            <button
+              onMouseEnter={() => {
+                setIsOpenDroplink1(true);
+              }}
+              onMouseLeave={() => {
+                setIsOpenDroplink1(false);
+              }}
+              className="pl-5 py-2 flex flex-row items-center
+             text-white rounded hover:opacity-80"
+            >
+              Product
+              <svg
+                className={`w-2.5 h-2.5 ml-2 ${
+                  isOpenDroplink1
+                    ? "translate-y-[2.5px]"
+                    : "translate-y-[0.5px]"
+                } text-gray-400 transition-transform duration-300`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
             </button>
-          </form>
+
+            {isOpenDroplink1 && (
+              <div
+                onMouseEnter={() => {
+                  setIsOpenDroplink1(true);
+                }}
+                onMouseLeave={() => {
+                  setIsOpenDroplink1(false);
+                }}
+                className="absolute top-8 -right-14 mt-2 w-48 bg-white rounded-lg shadow-lg"
+              >
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white"
+                >
+                  Option 1
+                </a>
+              </div>
+            )}
+          </div>
+
+          <div className="relative">
+            <button
+              onMouseEnter={() => {
+                setIsOpenDroplink2(true);
+              }}
+              onMouseLeave={() => {
+                setIsOpenDroplink2(false);
+              }}
+              className="pl-5 py-2 flex flex-row items-center
+             text-white rounded hover:opacity-80"
+            >
+              Solutions
+              <svg
+                className={`w-2.5 h-2.5 ml-2 ${
+                  isOpenDroplink2
+                    ? "translate-y-[2.5px]"
+                    : "translate-y-[0.5px]"
+                } text-gray-400 transition-transform duration-300`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            {isOpenDroplink2 && (
+              <div
+                onMouseEnter={() => {
+                  setIsOpenDroplink2(true);
+                }}
+                onMouseLeave={() => {
+                  setIsOpenDroplink2(false);
+                }}
+                className="absolute top-8 -right-14 mt-2 w-48 bg-white rounded-lg shadow-lg"
+              >
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800  hover:text-white"
+                >
+                  Option 1
+                </a>
+              </div>
+            )}
+          </div>
+
+          <div className="relative">
+            <button
+              className="pl-5 py-2 flex flex-row items-center
+             text-white rounded hover:opacity-80"
+            >
+              Pricing
+            </button>
+          </div>
+          {/* Close Dropdown Link */}
         </div>
-      </nav>
+
+        <div className="flex items-center justify-between ">
+          {/* Searching */}
+          <div className="sm:w-64 lg:w-80 py-3 pr-4 ">
+            <form
+              className="flex flex-row items-center relative"
+              onSubmit={handleSubmit}
+            >
+              <input
+                className="h-9 pl-10 pr-2 text-sm text-white border-2 border-gray-300 rounded-md bg-transparent absolute right-0 left-0"
+                placeholder="Judul lagu..."
+                onChange={handleInputChange}
+                value={inputValue}
+                type="text"
+              />
+              <button className="z-10" type="submit">
+                <FaSearch size={16} color="white" className="ml-2" />
+              </button>
+            </form>
+          </div>
+          {/* Close Searching */}
+
+          {/* Sign Up Button */}
+          <div className="pr-4">
+            <button className="px-2 py-1 rounded-md text-white hover:opacity-80">
+              Sign in
+            </button>
+          </div>
+          {/* Close Sign Up Button */}
+
+          {/* Sign Up Button */}
+          <div>
+            <button className="border px-2 py-1 rounded-md text-white hover:opacity-80">
+              Sign up
+            </button>
+          </div>
+          {/* Close Sign Up Button */}
+        </div>
+      </div>
+      {/* Close Navbar Website Site */}
     </div>
   );
 }
